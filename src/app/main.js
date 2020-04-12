7b9c3ef0bbe949d60a657949ad760b07a7043c73
范轶洁 <fanyjb@digitalchina.com>
2020-04-12 20:55:37 +0800

    floor-plan

--- src/app/main.js
+++ src/app/main.js
@@ -1,16 +1,10 @@
-var accessToken =
-  "pk.eyJ1IjoiZmFuY24yMXRoIiwiYSI6ImNrOHJhZXh6cDBja3AzZW80enBmZmw5cG8ifQ.nXK16O-pBFoD12BcyS9uzQ";
-var map = L.map("map").setView([37.8, -96], 4);
+var map = L.map("map", {
+  crs: L.CRS.Simple,
+});
 
-L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" +
-    accessToken,
-  {
-    id: "mapbox/light-v9",
-    attribution: "",
-    tileSize: 512,
-    zoomOffset: -1,
-  }
-).addTo(map);
-
-L.geoJson(statesData).addTo(map);
+var bounds = [
+  [0, 0],
+  [1000, 1000],
+];
+var image = L.imageOverlay("assets/image/floor-pan.svg", bounds).addTo(map);
+map.fitBounds(bounds);

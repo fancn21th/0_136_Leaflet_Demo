0941f2600fd69ca83bbec72beed308875cd6e3c8
范轶洁 <fanyjb@digitalchina.com>
2020-04-12 17:23:44 +0800

    Choropleth Map

--- src/app/main.js
+++ src/app/main.js
@@ -1,16 +1,16 @@
-var map = L.map("mapid").setView([30.5928, 114.3055], 13);
 var accessToken =
   "pk.eyJ1IjoiZmFuY24yMXRoIiwiYSI6ImNrOHJhZXh6cDBja3AzZW80enBmZmw5cG8ifQ.nXK16O-pBFoD12BcyS9uzQ";
-// tile layer
+var map = L.map("map").setView([37.8, -96], 4);
+
 L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" +
+    accessToken,
   {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: "mapbox/streets-v11",
+    id: "mapbox/light-v9",
+    attribution: "",
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: accessToken,
   }
 ).addTo(map);
+
+L.geoJson(statesData).addTo(map);
